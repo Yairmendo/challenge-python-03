b0ab47d37d4bdc77f0cf74f39e010742e502cfe1
Yair Mendoza <yairmendo@gmail.com>
2020-04-20 17:24:18 -0500

    Reto resuelto

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 # Resolve the problem!!
+import re
 
 
 def run():
-    # Start coding here
-
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        message = f.read()
+    
+    pattern = re.compile('[a-z]')
+    decryp_message = re.findall(pattern, message)
+    
+    solution = ''.join(decryp_message)
+    print(solution)
+ 
 
 if __name__ == '__main__':
     run()
